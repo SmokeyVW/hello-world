6ece19d251419456570d080f1e606a16802d04c8
SmokeyVW <smokeyvw-github@kats22.com>
2016-09-18 12:33:51 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -11,7 +11,13 @@ int main (int argc, char **argv)
   fprintf(stdout,"start\n");
 
   for (i = 1; i < argc; i++) {
+    if (strcmp(argv[i],"-foo") == 0) {
+      fprintf(stdout,"saw arg: '%s' and ignored it\n",argv[i]);
+      continue;
+    }
     fprintf(stdout,"unknown arg: '%s'\n", argv[i]);
+    fprintf(stdout,"stopping on error\n");
+    exit(1);
   }
 
   fprintf(stdout,"normal exit\n");
